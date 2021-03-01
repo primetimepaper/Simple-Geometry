fa0140825537e21282c2b66ca33e7156c55ec5b4
Pratham <72558963+primetimepaper@users.noreply.github.com>
2021-03-01 13:16:57 +0000

    Update Square.java

--- simple_geometry/Square.java
+++ simple_geometry/Square.java
@@ -7,29 +7,20 @@ public class Square extends Rectangle{
     }
 
     public Square(double s){
-        super.setWidth(s);
-        super.setLength(s);
+        super(s, s);
     }
 
     public Square(Point p, double s){
-        super.setWidth(s);
-        super.setLength(s);
-        super.centre = p;
+        super(s, s);
+        super.setCentre(p);
     }
 
     public Square(double s, String c, boolean f){
-        super.setWidth(s);
-        super.setLength(s);
-        super.setColor(c);
-        super.setFilled(f);
+        super(s, s, c, f);
     }
 
     public Square(Point p, double s, String c, boolean f){
-        super.setWidth(s);
-        super.setLength(s);
-        super.setColor(c);
-        super.setFilled(f);
-        super.centre = p;
+        super(p, s, s, c, f);
     }
 
     public double getSide(){
@@ -41,24 +32,73 @@ public class Square extends Rectangle{
         super.setLength(s);
     }
 
+    public String getEdges(){
+        return super.getEdges();
+    }
+
+    public double getArea(){
+        return super.getArea();
+    }
+
+    public double getPerimeter(){
+        return super.getPerimeter();
+    }
+
+    public void MoveDown(){
+        super.MoveDown();
+    }
+
+    public void MoveUp(){
+        super.MoveUp();
+    }
+
+    public void MoveLeft(){
+        super.MoveLeft();
+    }
+
+    public void MoveRight(){
+        super.MoveRight();
+    }
+
+    public void MoveTo(Point p){
+        super.MoveTo(p);
+    }
+
     @Override
     public void setWidth(double s){
         super.setWidth(s);
         super.setLength(s);
     }
-
+    
+    @Override
     public void setLength(double s){
         super.setWidth(s);
         super.setLength(s);
     }
 
+    @Override
     public String toString(){
         String tostr = super.toString();
         String x = "A Square with side " + this.getSide() + ", which is a subclass of " + tostr;
         return x;
     }
+    @Override
+    public boolean equals(Object other){
+        if(other instanceof Square){
+            Square otherSquare = (Square) other;
+            this.getEdges();
+            otherSquare.getEdges();
+            for(int i = 0; i < 4; i++){
+                if(!(this.edge[i].equals(otherSquare.edge[i]))){
+                    return false;
+                }
+            }
+            return true;
+        }
+        else{return false;}
+    }
 
-    public void resize(double percent){
-        super.resize(percent);
+    public void Resize(double percent){
+        super.Resize(percent);
     }
 }
